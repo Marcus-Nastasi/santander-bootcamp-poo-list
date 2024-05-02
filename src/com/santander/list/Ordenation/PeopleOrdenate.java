812560b60720a5b0a3368c2bb9025fec8e67f0cd
rolemberg <vinnie.nsts@gmail.com>
2024-05-02 20:04:09 -0300

    created PeopleOrdenate class;

---
+++ src/com/santander/list/Ordenation/PeopleOrdenate.java
@@ -0,0 +1,19 @@
+package com.santander.list.Ordenation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PeopleOrdenate {
+
+    private final List<People> peopleList = new ArrayList<>();
+
+    public PeopleOrdenate() {}
+
+    public void addPeople(People people) {
+        peopleList.add(people);
+    }
+
+    public void ordenateByAge() {
+
+    }
+}
