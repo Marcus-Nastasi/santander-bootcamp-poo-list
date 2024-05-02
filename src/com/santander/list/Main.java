d8df10e3c9229a8cab166159521c697b4b419bb7
rolemberg <vinnie.nsts@gmail.com>
2024-05-02 13:10:59 -0300

    testing program;

--- src/com/santander/list/Main.java
+++ src/com/santander/list/Main.java
@@ -1,8 +1,21 @@
 package com.santander.list;
 
+import com.santander.list.OpBasics.Task;
+import com.santander.list.OpBasics.TaskList;
+
 public class Main {
     public static void main(String[] args) {
+        Task t1 = new Task("test program");
+        Task t2 = new Task("other task");
+        Task t3 = new Task("third one");
 
+        TaskList taskList = new TaskList();
+
+        taskList.addTask(t1);
+        taskList.addTask(t2);
+        taskList.addTask(t3);
+
+        taskList.getAllTasks();
     }
 }
 
--- src/com/santander/list/OpBasics/TaskList.java
+++ src/com/santander/list/OpBasics/TaskList.java
@@ -5,19 +5,20 @@ import java.util.ArrayList;
 public class TaskList {
 
     Task task;
-    ArrayList<Task> tasks;
+    ArrayList<Task> tasks = new ArrayList<>();
 
     public TaskList(ArrayList<Task> tasks) {
         this.tasks = tasks;
     }
 
+    public TaskList() {}
+
     public ArrayList<Task> getTasks() {
         return tasks;
     }
 
-    public void addTask(String description) {
-        Task t = new Task(description);
-        tasks.add(t);
+    public void addTask(Task task) {
+        this.tasks.add(task);
     }
 
     public void removeTask(Task task) {
